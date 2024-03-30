f8d3c5a3a7467ea8fccf69c2cec5930e1f798d35
Valentin <vpavonlopez@gmail.com>
2024-03-31 00:46:40 +0100

    codigo qr para el inicio de sesion implementado

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,22 @@
 <template>
-  <section class="flex items-center justify-center w-[100%] h-[100vh] bg-[#212121]">
-      <div class="flex flex-col  items-center justify-around  rounded-lg w-[350px] h-[650px]">
+  <section class="flex items-center justify-center w-[100%] md:h-[100vh] bg-[#212121]">
+      <div class="flex flex-col  items-center justify-around  rounded-lg w-[350px] h-[750px]">
           <img :src="require('/Users/valentinpavonlopez/Desktop/TFG/tfgfrontend/public/img/logo.png')" alt="">
-          <div class="w-[300px] h-[50px] rounded-2xl flex items-center bg-white p-1 justify-center font-bold cursor-pointer">
-            <img src="https://rotulosmatesanz.com/wp-content/uploads/2017/09/2000px-Google_G_Logo.svg_.png" alt="" class="w-[30px]">
-            <p class="ml-2">Iniciar sesion con Google</p>
-          </div>
-          <p class="text-white">Or</p>
-          <input type="text" class="w-[300px] h-[50px] text-[20px] rounded-2xl text-white outline-none bg-black p-2" placeholder="Nombre de usuario">
-          <input type="passsword" class="w-[300px] h-[50px] text-[20px] rounded-2xl text-white outline-none bg-black p-2" placeholder="Contraseña">
-          <button class="text-white bg-[#287EFF] w-[300px] h-[50px] text-[20px] rounded-2xl" >Iniciar sesion</button>
+          <QRCodeVue3 value="http://localhost:8080/login" :qrOptions="{ typeNumber: '0', mode: 'Byte', errorCorrectionLevel: 'Q' }" :imageOptions="{ hideBackgroundDots: true, imageSize: 0.4, margin: 0 }" :dotsOptions="{ type: 'square', color: 'black' }" :cornersSquareOptions="{ type: 'square', color: 'black' }"/>
+          <p class="text-white font-bold text-center">Escanea el código qr para obtener el código de verificación e iniciar sesión.</p>
+          <button class="text-white bg-[#287EFF] w-[300px] h-[50px] text-[20px] rounded-2xl" >Verificar codigo</button>
           <hr class="w-[85%]">
-          <p class="text-white" @click="register">No estas registrado? <span class="text-[#287EFF] cursor-pointer">Registrarse</span></p>
+          <p class="text-white" @click="register">¿No estás registrado? <span class="text-[#287EFF] cursor-pointer">Registrarse</span></p>
       </div>
   </section>
 </template>
 <script>
+import QRCodeVue3 from 'qrcode-vue3';
 export default{
   name:'LoginView',
+  components:{
+    QRCodeVue3
+  },
   methods:{
     register(){
       this.$router.push({name:'register'})
