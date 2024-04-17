13967143d4a8a432ef484ed0081b19baf40ec6d6
Valentin <vpavonlopez@gmail.com>
2024-04-17 16:58:59 +0200

    Introducido libreria de pop-ups

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <router-view/>
-</template>
-<style lang="sass">
-*
-  font-family: "Montserrat", sans-serif
-</style>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import VueTelInput from 'vue-tel-input';
-import 'vue-tel-input/dist/vue-tel-input.css';
-
-
-createApp(App).use(store).use(VueTelInput).use(router).mount('#app')
